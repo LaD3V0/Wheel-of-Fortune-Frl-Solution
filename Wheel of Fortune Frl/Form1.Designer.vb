db6322436a1481a79ee782b091ac875f4e800ca1
DEVIN CARTER <DCarter7101@slps.org>
2023-05-02 09:18:23 -0500

    it can spin

--- Wheel of Fortune Frl/Form1.Designer.vb
+++ Wheel of Fortune Frl/Form1.Designer.vb
@@ -22,29 +22,96 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnSpin = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(428, 188)
+        Me.Button1.Location = New System.Drawing.Point(55, 210)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'btnSpin
+        '
+        Me.btnSpin.Location = New System.Drawing.Point(55, 167)
+        Me.btnSpin.Name = "btnSpin"
+        Me.btnSpin.Size = New System.Drawing.Size(75, 23)
+        Me.btnSpin.TabIndex = 102
+        Me.btnSpin.Text = "&Spin"
+        Me.btnSpin.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "wheel 26 unsel.png")
+        Me.ImageList1.Images.SetKeyName(1, "wheel 25.png")
+        Me.ImageList1.Images.SetKeyName(2, "wheel 24.png")
+        Me.ImageList1.Images.SetKeyName(3, "wheel 23.png")
+        Me.ImageList1.Images.SetKeyName(4, "wheel 22.png")
+        Me.ImageList1.Images.SetKeyName(5, "wheel 21.png")
+        Me.ImageList1.Images.SetKeyName(6, "wheel 20.png")
+        Me.ImageList1.Images.SetKeyName(7, "wheel 19.png")
+        Me.ImageList1.Images.SetKeyName(8, "wheel 18.png")
+        Me.ImageList1.Images.SetKeyName(9, "wheel 17.png")
+        Me.ImageList1.Images.SetKeyName(10, "wheel 16.png")
+        Me.ImageList1.Images.SetKeyName(11, "wheel 15.png")
+        Me.ImageList1.Images.SetKeyName(12, "wheel 14.png")
+        Me.ImageList1.Images.SetKeyName(13, "wheel 13.png")
+        Me.ImageList1.Images.SetKeyName(14, "wheel 12.png")
+        Me.ImageList1.Images.SetKeyName(15, "wheel 11.png")
+        Me.ImageList1.Images.SetKeyName(16, "wheel 10.png")
+        Me.ImageList1.Images.SetKeyName(17, "wheel 9.png")
+        Me.ImageList1.Images.SetKeyName(18, "wheel 8.png")
+        Me.ImageList1.Images.SetKeyName(19, "wheel 7.png")
+        Me.ImageList1.Images.SetKeyName(20, "wheel 6.png")
+        Me.ImageList1.Images.SetKeyName(21, "wheel 5.png")
+        Me.ImageList1.Images.SetKeyName(22, "wheel 4.png")
+        Me.ImageList1.Images.SetKeyName(23, "wheel 3.png")
+        Me.ImageList1.Images.SetKeyName(24, "wheel 2.png")
+        Me.ImageList1.Images.SetKeyName(25, "wheel 1.png")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(198, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(884, 547)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 103
+        Me.PictureBox1.TabStop = False
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.ClientSize = New System.Drawing.Size(1220, 658)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnSpin)
         Me.Controls.Add(Me.Button1)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmMain"
         Me.Text = "MainForm"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Button1 As Button
+    Friend WithEvents btnSpin As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Wheel of Fortune Frl/Form1.vb
+++ Wheel of Fortune Frl/Form1.vb
@@ -1,6 +1,80 @@
 ﻿Public Class frmMain
+    Dim spinvalue As Integer
+    Dim num As Integer = 0
+    Dim newnum As Integer
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Form2Wheel.Visible = True
 
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        spinvalue = Int(Rnd() * 101 + 50)
+        num += 1
+        newnum += spinvalue
+        If num = 0 Then
+            PictureBox1.Image = My.Resources.wheel_1
+        ElseIf num = 1 Then
+            PictureBox1.Image = My.Resources.wheel_2
+        ElseIf num = 2 Then
+            PictureBox1.Image = My.Resources.wheel_3
+        ElseIf num = 3 Then
+            PictureBox1.Image = My.Resources.wheel_4
+        ElseIf num = 4 Then
+            PictureBox1.Image = My.Resources.wheel_5
+        ElseIf num = 5 Then
+            PictureBox1.Image = My.Resources.wheel_6
+        ElseIf num = 6 Then
+            PictureBox1.Image = My.Resources.wheel_7
+        ElseIf num = 7 Then
+            PictureBox1.Image = My.Resources.wheel_8
+        ElseIf num = 8 Then
+            PictureBox1.Image = My.Resources.wheel_9
+        ElseIf num = 9 Then
+            PictureBox1.Image = My.Resources.wheel_10
+        ElseIf num = 10 Then
+            PictureBox1.Image = My.Resources.wheel_11
+        ElseIf num = 11 Then
+            PictureBox1.Image = My.Resources.wheel_12
+        ElseIf num = 12 Then
+            PictureBox1.Image = My.Resources.wheel_13
+        ElseIf num = 13 Then
+            PictureBox1.Image = My.Resources.wheel_14
+        ElseIf num = 14 Then
+            PictureBox1.Image = My.Resources.wheel_15
+        ElseIf num = 15 Then
+            PictureBox1.Image = My.Resources.wheel_16
+        ElseIf num = 16 Then
+            PictureBox1.Image = My.Resources.wheel_17
+        ElseIf num = 17 Then
+            PictureBox1.Image = My.Resources.wheel_18
+        ElseIf num = 18 Then
+            PictureBox1.Image = My.Resources.wheel_19
+        ElseIf num = 19 Then
+            PictureBox1.Image = My.Resources.wheel_20
+        ElseIf num = 20 Then
+            PictureBox1.Image = My.Resources.wheel_21
+        ElseIf num = 21 Then
+            PictureBox1.Image = My.Resources.wheel_22
+        ElseIf num = 22 Then
+            PictureBox1.Image = My.Resources.wheel_23
+        ElseIf num = 23 Then
+            PictureBox1.Image = My.Resources.wheel_24
+        ElseIf num = 24 Then
+            PictureBox1.Image = My.Resources.wheel_25
+        End If
+        If num = 25 Then
+            num = 0
+        End If
+
+    End Sub
+
+    Private Sub btnSpin_Click(sender As Object, e As EventArgs) Handles btnSpin.Click
+        Timer1.Enabled = True
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
     End Sub
 End Class
