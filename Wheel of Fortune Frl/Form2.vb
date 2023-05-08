8096261d026add50e270899f4a01a43eccfc0132
DEVIN CARTER <DCarter7101@slps.org>
2023-05-08 10:02:28 -0500

    Created a "check for the letter" idea so that buttons check for the letter you clicked *not finished*

--- Wheel of Fortune Frl/Form2.vb
+++ Wheel of Fortune Frl/Form2.vb
@@ -10,15 +10,18 @@ Public Class Form2Wheel
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If Label1.Text.Trim.ToUpper Like "[A-Z][A-Z][A-Z][A-Z][A-Z]" Then
+        'If Label1.Text.Trim.ToUpper Like "[A-Z][A-Z][A-Z][A-Z][A-Z]" Then
 
-            'grpWord.Enabled = False
-            'grpLetter.Enabled = True
-            'lblResult.Text = "-----"
-            'txtLetter.Focus()
-        Else
-            MessageBox.Show("Please enter 5 letters.", "Guess the Word Game",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information)
-        End If
+        '    'grpWord.Enabled = False
+        '    'grpLetter.Enabled = True
+        '    'lblResult.Text = "-----"
+        '    'txtLetter.Focus()
+        'Else
+        '    MessageBox.Show("Please enter 5 letters.", "Guess the Word Game",
+        '                    MessageBoxButtons.OK, MessageBoxIcon.Information)
+        'End If
+    End Sub
+    Private Sub LetterCheck(ByVal itsChecked As String)
+
     End Sub
 End Class
